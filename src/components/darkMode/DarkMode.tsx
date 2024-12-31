ff8b75347769c3e788acb564e6db1efa3325b2b3
Minyeong Jeong <megormin@gmail.com>
2024-12-31 16:22:48 +0100

    dark mode bg added

--- src/components/darkMode/DarkMode.tsx
+++ src/components/darkMode/DarkMode.tsx
@@ -1,9 +1,17 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import "./DarkMode.css";
 import { DarkModeContext } from "../../context/Context";
 
 const DarkMode = () => {
   const { darkMode, setDarkMode } = useContext(DarkModeContext);
+  useEffect(() => {
+    const body = document.body;
+    if (darkMode) {
+      body.classList.add("dark-mode");
+    } else {
+      body.classList.remove("dark-mode");
+    }
+  }, [darkMode]);
   return (
     <div className="d-l-mode" onClick={() => setDarkMode(!darkMode)}>
       {darkMode ? "Sun" : "Moon"}
